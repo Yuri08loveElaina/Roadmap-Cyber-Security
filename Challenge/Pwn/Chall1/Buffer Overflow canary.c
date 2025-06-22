12c4b5765d509353335c3fc3e85c48c2cf690c61
YURI08 <147913818+Yuri08loveElaina@users.noreply.github.com>
2025-06-22 14:13:07 +0700

    Create Buffer Overflow canary.c

---
+++ Challenge/Pwn/Chall1/Buffer Overflow canary.c
@@ -0,0 +1,22 @@
+// pwn2_bof_canary.c
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+void win() {
+    system("/bin/sh");
+}
+
+void vuln() {
+    char buf[64];
+    printf("Input something: ");
+    fgets(buf, 256, stdin);
+    printf("You entered: %s\n", buf);
+}
+
+int main() {
+    setbuf(stdout, NULL);
+    vuln();
+    printf("Bye!\n");
+    return 0;
+}
